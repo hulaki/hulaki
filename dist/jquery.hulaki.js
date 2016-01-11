81872dc6306e149206bf1a194dc6ac0431bc11ba
sushil_dahal <sushil.dahal@techguthi.com>
2016-01-11 12:13:13 +0545

    Adds dist

---
+++ dist/jquery.hulaki.js
@@ -0,0 +1,33 @@
+/*! Hulaki - v0.1.0 - 2016-01-11
+* https://github.com/hulaki/hulaki
+* Copyright (c) 2016 hulaki; Licensed MIT */
+(function($) {
+
+  // Collection method.
+  $.fn.hulaki = function() {
+    return this.each(function(i) {
+      // Do something awesome to each selected element.
+      $(this).html('awesome' + i);
+    });
+  };
+
+  // Static method.
+  $.hulaki = function(options) {
+    // Override default options with passed-in options.
+    options = $.extend({}, $.hulaki.options, options);
+    // Return something awesome.
+    return 'awesome' + options.punctuation;
+  };
+
+  // Static method default options.
+  $.hulaki.options = {
+    punctuation: '.'
+  };
+
+  // Custom selector.
+  $.expr[':'].hulaki = function(elem) {
+    // Is this element awesome?
+    return $(elem).text().indexOf('awesome') !== -1;
+  };
+
+}(jQuery));
